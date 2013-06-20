d0b060099d73734fcdb45d8a6832450b0ab7d41d
Matt Sanders <matt@modal.org>
2013-06-20 14:47:28 -0700

    Tweak test nesting

--- test/unit/logreporter/configuration_test.rb
+++ test/unit/logreporter/configuration_test.rb
@@ -2,29 +2,31 @@ require 'test_helper'
 require 'stringio'
 
 module Librato
-  class LogReporterConfigurationTest < MiniTest::Test
+  class LogReporter
+    class ConfigurationTest < MiniTest::Test
 
-    def setup
-      @reporter = LogReporter.new
+      def setup
+        @reporter = LogReporter.new
+      end
+
+      def test_default_log
+        assert_equal $stdout, @reporter.log
+      end
+
+      def test_setting_log
+        @reporter.log = $stderr
+        assert_equal $stderr, @reporter.log
+      end
+
+      def test_default_source
+        assert_equal nil, @reporter.source
+      end
+
+      def test_setting_source
+        @reporter.source = 'librato'
+        assert_equal 'librato', @reporter.source
+      end
+
     end
-
-    def test_default_log
-      assert_equal $stdout, @reporter.log
-    end
-
-    def test_setting_log
-      @reporter.log = $stderr
-      assert_equal $stderr, @reporter.log
-    end
-
-    def test_default_source
-      assert_equal nil, @reporter.source
-    end
-
-    def test_setting_source
-      @reporter.source = 'librato'
-      assert_equal 'librato', @reporter.source
-    end
-
   end
 end
