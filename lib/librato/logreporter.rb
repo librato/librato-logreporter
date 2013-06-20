e8b6b8c7281dfabc0eb193b293087e23f9bdc706
Matt Sanders <matt@modal.org>
2013-06-20 14:23:14 -0700

    Measure/timing support

--- lib/librato/logreporter.rb
+++ lib/librato/logreporter.rb
@@ -27,9 +27,49 @@ module Librato
       @log = io
     end
 
+    # @example Simple measurement
+    #   measure 'sources_returned', sources.length
+    #
+    # @example Simple timing in milliseconds
+    #   timing 'myservice.lookup', 2.31
+    #
+    # @example Block-based timing
+    #   timing 'db.query' do
+    #     do_my_query
+    #   end
+    #
+    # @example Custom source
+    #    measure 'user.all_orders', user.order_count, :source => user.id
+    #
+    def measure(*args, &block)
+      options = {}
+      event = args[0].to_s
+      returned = nil
+
+      # handle block or specified argument
+      if block_given?
+        start = Time.now
+        returned = yield
+        value = ((Time.now - start) * 1000.0).to_i
+      elsif args[1]
+        value = args[1]
+      else
+        raise "no value provided"
+      end
+
+      # detect options hash if present
+      if args.length > 1 and args[-1].respond_to?(:each)
+        options = args[-1]
+      end
+
+      log_write(event => value, :source => options[:source])
+      returned
+    end
+    alias :timing :measure
+
     # current default source
     def source
-      @source
+      @source ||= ENV['LIBRATO_SOURCE']
     end
 
     # set default source
--- test/unit/logreporter_test.rb
+++ test/unit/logreporter_test.rb
@@ -30,6 +30,30 @@ module Librato
       assert_last_logged 'measure.days.foggy=1 source=seattle'
     end
 
+    def test_measure
+      @reporter.measure 'documents.rendered', 12
+      assert_last_logged 'measure.documents.rendered=12'
+
+      # custom source
+      @reporter.measure 'cycles.wasted', 23, :source => 'cpu_1'
+      assert_last_logged 'measure.cycles.wasted=23 source=cpu_1'
+    end
+
+    def test_timing
+      @reporter.timing('do.stuff') { sleep 0.1 }
+      last = last_logged
+      assert last =~ /\=/, 'should have a measure pair'
+      key, value = last.split('=')
+      assert_equal 'measure.do.stuff', key, 'should have timing key'
+      assert_in_delta 100, value.to_i, 10
+
+      # custom source
+      @reporter.timing('do.more', :source => 'worker') do
+        sleep 0.05
+      end
+      assert last_logged.index('source=worker'), 'should use custom source'
+    end
+
     private
 
     def assert_last_logged(string)
@@ -38,7 +62,7 @@ module Librato
 
     def last_logged
       @buffer.rewind
-      @buffer.readlines[-1].chomp
+      @buffer.readlines[-1].to_s.chomp
     end
 
   end
